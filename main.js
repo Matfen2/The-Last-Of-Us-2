62937c2139b56d79221cd210c564bb578176bf9d
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2024-03-13 10:10:23 +0100

    Update main.js
    Code plus compréhensible, plus pertinant

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const gamesTlou = [
 ];
 
 const listGames = document.querySelector('.listGames');
+const body = document.getElementById('contain');
 
 gamesTlou.forEach(game => {
     const img = document.createElement('img');
@@ -11,13 +12,12 @@ gamesTlou.forEach(game => {
     img.classList.add("img");
     listGames.appendChild(img);
 
-    // Ajout du nom du jeu en tant que texte alternatif
     img.alt = game.name;
 
     // Ajout d'un événement de survol pour changer la bordure
     img.addEventListener('mouseenter', function () {
         img.style.border = "2px solid azure";
-        body.style.backgroundImage = `url(${game.wallpaper})`;
+        body.style.backgroundImage = `url('${game.wallpaper}')`;
         body.style.backgroundRepeat = "no-repeat";
         body.style.backgroundSize = "cover";
         body.style.backgroundPosition = "center";
@@ -28,21 +28,5 @@ gamesTlou.forEach(game => {
     });
 });
 
-const part1 = document.querySelector('.part1');
-const part2 = document.querySelector('.part2');
-
-const body = document.getElementById('contain')
-
-part1.addEventListener('mouseenter', function () {
-    const wallaperPart1 = document.createElement("imgPart1");
-    imgPart1.src = "pict/theLastOfUsPart1Menu.jpg";
-    body.appendChild(wallaperPart1);
-})
-
-part2.addEventListener('mouseenter', function () {
-    const wallaperPart1 = document.createElement("imgPart2");
-    imgPart2.src = "pict/theLastOfUsPart2Menu.jpg";
-    body.appendChild(wallaperPart2);
-})
 
 
